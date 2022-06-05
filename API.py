93fc482476441024e4a44b1f0740aafb7c07fa93
yerp159 <101734432+yerp159@users.noreply.github.com>
2022-06-04 21:54:23 -0400

    Additions of the api keys

---
+++ API.py
@@ -0,0 +1,11 @@
+import tweepy
+import keys
+
+client = tweepy.Client(
+    consumer_key = keys.consumer_key,
+    consumer_secret=keys.consumer_secret,
+    access_token=keys.access_token,
+    access_token_secret=keys.access_token_secret
+)
+
+client.create_tweet(text="Hello")
