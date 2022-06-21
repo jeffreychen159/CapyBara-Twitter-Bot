b0f33cce7043c0f4cb2a48af92a83490b9ff10c7
yerp159 <101734432+yerp159@users.noreply.github.com>
2022-06-21 15:22:48 -0400

    returns lists instead of class

--- userinfo.py
+++ userinfo.py
@@ -3,12 +3,28 @@ import API
 class UserInfo(): 
     def __init__(self, userID) -> None:
         self.userID = userID
-        self.client = API.client
+        self.client = API.client 
 
-    def get_following_users(self): 
-        return self.client.get_users_followers(self.userID)
 
-    def get_tweet_mentions(self): 
-        return self.client.get_users_mentions(self.userID)
+    def get_followers(self): 
+        followers = self.client.get_users_followers(self.userID, user_auth=True)
+        follower_list = []
+        for follower in followers.data: 
+            follower_list.append(follower)
+        return follower_list
 
-    
+
+    def get_tweet_mentions_txt(self): 
+        mentions = self.client.get_users_mentions(self.userID, user_auth=True)
+        mentions_txt = []
+        for mention in mentions.data: 
+            mentions_txt.append(mention.txt)
+        return mentions_txt
+
+
+    def get_tweet_mentions_id(self):
+        mentions = self.client.get_users_mentions(self.userID, user_auth=True)
+        mentions_id = []
+        for mention in mentions.data: 
+            mentions_id.append(mention.id)
+        return mentions_id
