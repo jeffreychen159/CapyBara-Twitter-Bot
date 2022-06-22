95dff6042712123d65aabc432b6499370c19bf1b
yerp159 <101734432+yerp159@users.noreply.github.com>
2022-06-22 00:06:31 -0400

    returns lists of users in ID form

--- tweetinfo.py
+++ tweetinfo.py
@@ -6,10 +6,23 @@ class TweetInfo():
         self.client = API.client
 
     def get_retweeters(self): 
-        return self.client.get_retweeters(self.tweetID)
+        retweeters = self.client.get_retweeters(self.tweetID, user_auth=True)
+        retweeters_id = []
+        for retweeter in retweeters.data: 
+            retweeters_id.append(retweeter)
+        return retweeters_id
 
     def get_likers(self): 
-        return self.client.get_liking_users(self.tweetID)
+        likes = self.client.get_liking_users(self.tweetID, user_auth=True)
+        likers_id = []
+        for likers in likes.data: 
+            likers_id.append(likers)
+        return likers_id
+        
 
     def get_quote_tweeters(self): 
-        return self.client.get_quote_tweets(self.tweetID)
+        quote_tweeters = self.client.get_quote_tweets(self.tweetID, user_auth=True)
+        quote_tweeters_ids = []
+        for quote_tweeter in quote_tweeters.data: 
+            quote_tweeters_ids.append(quote_tweeter)
+        return quote_tweeters_ids
