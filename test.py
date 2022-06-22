61243de5cea7fc18900db1396996da01063c071f
yerp159 <101734432+yerp159@users.noreply.github.com>
2022-06-22 00:08:27 -0400

    current tests with tweets, times and tweepy

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import API
 from datetime import datetime
+import tweepy
 
 client = API.client
 
@@ -7,12 +8,21 @@ now = datetime.now()
 
 print(now)
 
-def get_first_fact(text): 
-    with open(text, 'r') as f: 
-        line = f.readline()
-        rest_lines = f.readlines()
-    with open(text, 'w') as f: 
-        f.writelines(rest_lines)
-    return line
+#user = client.get_user(username="yolosoloyolo1", user_auth=True)
+#userid = user.data.id
+#mentions = client.get_users_mentions(userid, user_auth=True)
+#for mention in mentions.data: 
+    #print(mention.id)
 
-print(get_first_fact('capybara_facts.txt'))
+mention_id = []
+mention_txt = []
+tweets = client.get_users_mentions(id='3388138689', user_auth=True)
+print(tweets)
+for tweet in tweets.data: 
+    mention_txt.append(tweet.text)
+for tweet in tweets.data:
+    mention_id.append(tweet.id)
+
+
+#tweet = client.get_tweet(id='1538788393714335745', user_auth=True)
+#print(tweet)
