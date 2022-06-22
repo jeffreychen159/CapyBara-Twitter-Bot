7079be80e0c9d3cd3c348bf09bbb587fa57bca78
yerp159 <101734432+yerp159@users.noreply.github.com>
2022-06-22 18:32:38 -0400

    fact count added

--- facts.py
+++ facts.py
@@ -3,7 +3,7 @@ class Facts():
         self.input_text = input_text
         self.output_text = output_text
 
-    def get_first_fact(self): 
+    def get_fact(self): 
         with open(self.input_text, 'r') as f: 
             line = f.readline()
             rest_lines = f.readlines()
@@ -13,9 +13,11 @@ class Facts():
             print(line, file=f)
         return line
 
-    def get_random_fact(self): 
+    def get_fact_count(self): 
         with open(self.input_text, 'r') as f: 
-            line = f.readlines()
-            
-        
-
+            all_lines = f.readlines()
+        if len(all_lines) == 1: 
+            print("There is " + str(len(all_lines)) + " fact left.")
+        else: 
+            print("There are " + str(len(all_lines)) + " facts left.")
+        return
