39c16d447bfa2974aad56c6e93046420b98c500a
yerp159 <101734432+yerp159@users.noreply.github.com>
2022-06-22 23:20:11 -0400

    picks a message based on sentiment score

---
+++ get_reply.py
@@ -0,0 +1,17 @@
+import sentiment
+
+REPLY_POS_1 = 'Thanks for loving capybaras!'
+REPLY_POS_2 = 'Thanks for liking capybaras!'
+REPLY_NEG_1 = 'According to the bot, this is a negative comment.'
+REPLY_NEG_2 = 'L + Ratio'
+
+def auto_reply_message(message):
+    score = sentiment.sentiment_score(message)
+    if score[0] >= 0.75: 
+        return REPLY_POS_1
+    elif score[0] >= 0.5:
+        return REPLY_POS_2
+    elif score[0] >= 0.25:
+        return REPLY_NEG_1
+    else: 
+        return REPLY_NEG_2
